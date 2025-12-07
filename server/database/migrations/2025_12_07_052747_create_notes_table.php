2d29df7df1df6b7fe2de0b566b333c7ce66245f9
Shareiar <islamshareiar@gmail.com>
2025-12-07 12:03:23 +0600

    Add migrations for companies, workspaces, notes, and tags

---
+++ server/database/migrations/2025_12_07_052747_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('workspace_id')->constrained()->cascadeOnDelete();
+            $table->string('title')->index();
+            $table->longText('content');
+            $table->enum('type', ['public','private'])->index();
+            $table->enum('status', ['draft','published'])->index();
+            $table->unsignedInteger('upvotes_count')->default(0)->index();
+            $table->unsignedInteger('downvotes_count')->default(0)->index();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->index(['workspace_id','status','type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
